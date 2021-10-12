3f17e841cd7ac07b4673ee642a20e332f1e6be95
Leonard Martin <leonard.martin@gmail.com>
2021-10-12 10:45:16 +0100

    First commit of barebones UI code
    Adds a _very_ basic express webserver and minimal dependency set.

---
+++ ui/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res) => {
+  res.send('Hello World!');
+});
+
+const server = app.listen(process.env.PORT, (err, result) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(`Listening on port ${server.address().port}`);
+});
