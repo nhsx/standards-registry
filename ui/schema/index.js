2825834cbc8891782f7deaa3aeab694a6b51e783
Ralph Cowling <ralph@ralph-cowling.com>
2021-12-14 14:05:02 +0100

    format options for empty arrays

--- ui/schema/index.js
+++ ui/schema/index.js
@@ -1,6 +1,7 @@
 import upperFirst from 'lodash/upperFirst';
 import { Tag, Link, MarkdownBlock } from '../components';
 
+// `!!val?.length` => check whether empty array or unset val
 export default [
   {
     section_title: 'About this standard',
@@ -23,41 +24,55 @@ export default [
       label: 'Documentation',
       format: (val, data) => (
         <>
-          <MarkdownBlock md={val} />
-          <Link
-            href={data.documentation_link}
-            text="View documentation for this standard (opens in new window)"
-          />
+          {val && <MarkdownBlock md={val} />}
+          {data.documentation_link && (
+            <Link
+              href={data.documentation_link}
+              text="View documentation for this standard (opens in new window)"
+              newWindow={true}
+            />
+          )}
         </>
       ),
     },
   },
   {
     section_title: 'Business and care setting usage',
-    business_use: { label: 'Business use' },
+    business_use: {
+      label: 'Business use',
+      format: (val) => val || 'As yet unspecified',
+    },
     care_setting: {
       label: 'Care Setting',
+      format: (val) => val || 'As yet unspecified',
     },
   },
   {
     section_title: 'Relationships to other standards',
     dependencies: {
       label: 'Dependencies',
-      format: (val) => val && <MarkdownBlock md={val} />,
+      format: (val) =>
+        (!!val?.length && <MarkdownBlock md={val} />) ||
+        'Information unavailable',
     },
     related_standards: {
       label: 'Related Standards',
-      format: (val) => val && <MarkdownBlock md={val} />,
+      format: (val) =>
+        (!!val?.length && <MarkdownBlock md={val} />) ||
+        'Information unavailable',
     },
   },
   {
     section_title: 'Assurance and endorsements',
     assurance: {
       label: 'Assurance',
-      format: (val) => val && <MarkdownBlock md={val} />,
+      format: (val) =>
+        (!!val?.length && <MarkdownBlock md={val} />) || 'Not applicable',
     },
     endorsements: {
       label: 'Endorsements',
+      format: (val) =>
+        (!!val?.length && <MarkdownBlock md={val} />) || 'Not applicable',
     },
   },
 ];
