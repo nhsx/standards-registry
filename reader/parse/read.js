2546096445bf663dbd3c3d96a391671def69ee57
Ralph Cowling <ralph@ralph-cowling.com>
2021-12-16 17:41:33 +0100

    read and write processed json

--- reader/parse/read.js
+++ reader/parse/read.js
@@ -1,6 +1,5 @@
 // Read the spreadsheet from google and parse into values ready for CKAN
-
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 import { joinTitlesToValues, prepHeadings } from './index.js';
 
 const sheet = JSON.parse(
@@ -10,5 +9,5 @@ const { values } = sheet;
 
 const headings = prepHeadings(values.shift());
 const parsed = values.map((vals) => joinTitlesToValues(headings, vals));
-
+await writeFile('parsed.json', JSON.stringify(parsed));
 console.log(parsed);
