c2ee615621ac668e64172934d7f86bc43b374f50
Ralph Cowling <ralph@ralph-cowling.com>
2021-12-21 16:48:12 +0100

    update footer

--- ui/components/FeedbackFooter/index.js
+++ ui/components/FeedbackFooter/index.js
@@ -7,24 +7,12 @@ export default function FeedbackFooter() {
       </h2>
       <p className="nhsuk-u-font-size-16">
         To report a problem, suggest an enhancement or propose a new feature,
-        you can:
+        you can email the standards directory team at{' '}
+        <Link
+          href="mailto:standards.directory@nhsx.nhs.uk"
+          text="standards.directory@nhsx.nhs.uk"
+        />
       </p>
-      <ul className="nhsuk-list-bullet nhsuk-u-font-size-16">
-        <li>
-          email the standards directory team on{' '}
-          <Link
-            href="mailto:standardsdirectory@nhsx.nhs.uk"
-            text="standardsdirectory@nhsx.nhs.uk"
-          />
-        </li>
-        <li>
-          raise an issue on the{' '}
-          <Link
-            href="https://github.com/Marvell-Consulting/standards-community-platform-for-health-and-social-care/projects/1"
-            text="standards community development backlog on GitHub"
-          />
-        </li>
-      </ul>
     </>
   );
 }
