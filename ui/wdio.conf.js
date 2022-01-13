ede79110b4505506de9dd07274bb8758e3466f25
Ralph Cowling <ralph@ralph-cowling.com>
2022-01-13 12:32:47 +0100

    no pinning

--- ui/wdio.conf.js
+++ ui/wdio.conf.js
@@ -112,7 +112,7 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: [['selenium-standalone', { drivers: { chrome: '95.0.4638.69' } }]],
+  services: [['selenium-standalone']],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
