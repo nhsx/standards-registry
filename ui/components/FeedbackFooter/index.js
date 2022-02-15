75a78f8bad37f9da2c34d3a4bda69451a99a978d
Ralph Cowling <ralph@ralph-cowling.com>
2022-02-15 13:05:24 +0100

    update footer

--- ui/components/FeedbackFooter/index.js
+++ ui/components/FeedbackFooter/index.js
@@ -6,12 +6,12 @@ export default function FeedbackFooter() {
         Feedback on this page
       </h2>
       <p className="nhsuk-u-font-size-16">
-        To report a problem, suggest an enhancement or propose a new feature,
-        you can email the standards directory team at{' '}
+        To report a problem or suggest an improvement, email{' '}
         <Link
           href="mailto:standards.directory@nhsx.nhs.uk"
           text="standards.directory@nhsx.nhs.uk"
         />
+        .
       </p>
     </>
   );
