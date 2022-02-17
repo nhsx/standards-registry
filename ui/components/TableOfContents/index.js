86693026ab71f154812628450fea1aac507166c2
Ralph Cowling <ralph@ralph-cowling.com>
2022-02-17 19:19:10 +0100

    parse markdown using mdast, generate TOC

--- ui/components/TableOfContents/index.js
+++ ui/components/TableOfContents/index.js
@@ -1,28 +1,41 @@
 import { Link } from '../';
+import { fromMarkdown } from 'mdast-util-from-markdown';
+
+const TocItem = ({ text }) => (
+  <li>
+    <Link
+      text={text}
+      href={'#' + text.toLowerCase().trim().replaceAll(/%20| /gm, '-')}
+    />
+  </li>
+);
+
+const TocList = ({ depth, text }) => {
+  return (
+    <>
+      {(depth - 2 >= 0 && (
+        <ul className="nhsuk-list-bullet">
+          <TocList depth={depth - 1} text={text} />
+        </ul>
+      )) || <TocItem text={text} />}
+    </>
+  );
+};
 
 export const TableOfContents = ({ content }) => {
-  // Anything h2 and below
-  const headingReg = new RegExp(/(#{2}\s)(.*)/gm);
-  const headings = content.match(headingReg);
-
+  const parsed = fromMarkdown(content);
+  const headings = parsed.children.filter((i) => i.type === 'heading');
   return (
     <div className="nhsuk-grid-column-one-third">
       <h3 className="nhsuk-heading-s">Contents</h3>
-      <ul className="nhsuk-list-bullet">
-        {headings.map((elem, i) => {
-          const heading = elem.replaceAll('#', '').trim();
-          return (
-            <li key={i}>
-              <Link
-                text={heading}
-                href={
-                  '#' + heading.toLowerCase().trim().replaceAll(/%20| /gm, '-')
-                }
-              />
-            </li>
-          );
-        })}
-      </ul>
+
+      {headings.map((heading, i) => {
+        const { depth, children } = heading;
+        const text = children
+          .reduce((i, child) => (i = [...i, child.value]), [])
+          .join(' ');
+        return <TocList text={text} depth={depth} key={i} />;
+      })}
     </div>
   );
 };
