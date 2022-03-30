24dfc37ec9f065d5c8e1097391c4849a72c23728
Leonard Martin <leonard.martin@gmail.com>
2022-03-30 16:33:23 +0100

    Linting cleanup

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -2,12 +2,8 @@ import Link from 'next/link';
 import {
   Hero,
   Layout,
-  Page,
   Snippet,
   Search,
-  Row,
-  Col,
-  Card,
 } from '../components';
 import styles from '../styles/Home.module.scss';
 import { getPages } from '../helpers/api';
@@ -21,42 +17,7 @@ const content = {
     'Use this directory to find nationally recognised information standards for interoperable technology in health and adult social care.',
 };
 
-const Section = (section, pages) => {
-  const items = pages.filter((i) => i.homepage_section === section);
-  return (
-    <React.Fragment key={section}>
-      <h3>{section}</h3>
-      <Row>
-        {items.map((pageItem, index) => {
-          const {
-            name,
-            title,
-            short_title: shortTitle,
-            homepage_snippet: snippet,
-            add_to_home_page: addToHomepage,
-          } = pageItem;
-          if (!addToHomepage) {
-            return null;
-          }
-          return (
-            <Col key={index}>
-              <Link href={`/${name}`}>
-                <a>
-                  <Card clickable className={styles.card}>
-                    <h5>{shortTitle || title}</h5>
-                    <p className="nhsuk-body-s">{snippet}</p>
-                  </Card>
-                </a>
-              </Link>
-            </Col>
-          );
-        })}
-      </Row>
-    </React.Fragment>
-  );
-};
-
-export default function Home({ pages }) {
+export default function Home() {
   return <>
     <div className="nhsuk-grid-row">
       <div className="nhsuk-grid-column-full">
