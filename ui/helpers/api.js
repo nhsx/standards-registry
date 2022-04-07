09ce5ae90c1e10950ff9a2244bdb8a4ce5ee10ad
Leonard Martin <leonard.martin@gmail.com>
2022-04-07 17:16:44 +0100

    Remove type mapping hack
    We will want to do this properly with a schema change and a migration.

--- ui/helpers/api.js
+++ ui/helpers/api.js
@@ -2,13 +2,6 @@ import fetch from 'node-fetch';
 import { stringify } from 'qs';
 const CKAN_URL = process.env.CKAN_URL;
 
-const typeMap = {
-  'Technical standards and specifications': 'Technical specifications and APIs',
-  'Record standard': 'Clinical and care record standards',
-  'Data definitions and terminologies': 'Medical data and dictionaries',
-  'Information code of practice and governance standard': 'Data governance and information'
-};
-
 // TODO: neaten
 export function queriseSelections(selections) {
   const selectionsRef = { ...selections };
@@ -48,9 +41,6 @@ export function serialise(obj = {}) {
 export async function read({ id }) {
   const response = await fetch(`${CKAN_URL}/package_show?id=${id}`);
   const data = await response.json();
-  if (typeMap[data.result.standard_category]) {
-    data.result.standard_category = typeMap[data.result.standard_category];
-  }
   return data.result;
 }
 
@@ -83,11 +73,7 @@ export async function list({ page = 1, q, sort, filters }) {
 
   const response = await fetch(`${CKAN_URL}/package_search?${ckanQuery}`);
   const data = await response.json();
-  data.result.results.forEach(record => {
-    if (typeMap[record.standard_category]) {
-      record.standard_category = typeMap[record.standard_category];
-    }
-  });
+
   return data.result;
 }
 
@@ -96,17 +82,6 @@ export async function schema(dataset = 'dataset') {
     `${CKAN_URL}/scheming_dataset_schema_show?type=${dataset}`
   );
   const data = await response.json();
-
-
-
-  const category = data.result.dataset_fields.find(field => field.field_name === 'standard_category');
-  if (category) {
-    category.choices.forEach(choice => {
-      if (typeMap[choice.value]) {
-        choice.label = typeMap[choice.value];
-      }
-    });
-  }
 
   return data.result;
 }
