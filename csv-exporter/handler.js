40ebb4c83546286defcae5ba2c3e71a7a76e604d
Ralph Cowling <ralph@ralph-cowling.com>
2022-04-19 18:20:41 +0200

    add throttling, remove the need for dotenv

--- csv-exporter/handler.js
+++ csv-exporter/handler.js
@@ -1,6 +1,5 @@
 'use strict';
 
-require('dotenv').config();
 const getAllDataSets = require('./get');
 const ObjectsToCsv = require('objects-to-csv');
 
@@ -16,50 +15,45 @@ const urlMap = (env) => urls[env] || urls['dev'];
 module.exports.datasets = async (event, context) => {
   let help = null;
   const { queryStringParameters } = event;
-  const download = queryStringParameters?.download === 'true';
+
   // * api key passed as key={key}
-  const { env, key } = queryStringParameters;
+  const { env, key = null, download = false } = queryStringParameters;
   const ckanUrl = urlMap(env);
 
   if (!key) {
-    help = `CKAN API Key not provided. Will proceed with non-authorised call to the api, which will result in private datasets being hidden.
-    The key can be provided in the format ?key={apiKey}`;
+    help = `CKAN API Key not provided. 
+Will proceed with non-authorised call to the api, but private datasets will be hidden.
+The key can be provided in the format ?key={apiKey}`;
   }
-
   try {
     const { results, count } = await getAllDataSets(ckanUrl, key);
     const csvResult = await new ObjectsToCsv(results).toString();
-    const status = { success: true, statusCode: 200, help };
     const response = {
-      ...status,
-      ...{
-        body: JSON.stringify(
-          {
-            // event,
-            // context,
-            result: {
-              count,
-              results,
-              csv: [csvResult],
-            },
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          help,
+          success: true,
+          result: {
+            count,
+            results,
+            csv: [csvResult],
           },
-          null,
-          2
-        ),
-      },
+        },
+        null,
+        2
+      ),
     };
 
     const downloadResponse = {
-      ...status,
-      ...{
-        headers: {
-          'Content-Type': 'application/octet-stream',
-          'Content-Length': csvResult.length,
-          'Content-Disposition': `inline; filename="datasets.test.csv"`,
-        },
-        isBase64Encoded: true,
-        body: Buffer.from(csvResult).toString('base64'),
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        'Content-Length': csvResult.length,
+        'Content-Disposition': `inline; filename="datasets.test.csv"`,
       },
+      isBase64Encoded: true,
+      body: Buffer.from(csvResult).toString('base64'),
     };
 
     return (download && downloadResponse) || response;
