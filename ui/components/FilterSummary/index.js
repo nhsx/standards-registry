64a8722e5ab7b931736564f505adcf8b4aa59edb
Ralph Cowling <ralph@ralph-cowling.com>
2022-05-16 15:46:42 +0200

    Nationally mandated => Mandatory in England

--- ui/components/FilterSummary/index.js
+++ ui/components/FilterSummary/index.js
@@ -1,14 +1,16 @@
 import { useQueryContext } from '../../context/query';
-import omit from 'lodash/omit'
-import without from 'lodash/without'
-import size from 'lodash/size'
+import omit from 'lodash/omit';
+import without from 'lodash/without';
+import size from 'lodash/size';
 
-import styles from './FilterSummary.module.scss'
+import styles from './FilterSummary.module.scss';
 
 function Widget({ children, onClick }) {
   return (
-    <div onClick={onClick} className={styles.widget}>X { children }</div>
-  )
+    <div onClick={onClick} className={styles.widget}>
+      X {children}
+    </div>
+  );
 }
 
 export function FilterSummary({ schema }) {
@@ -17,9 +19,9 @@ export function FilterSummary({ schema }) {
   function removeFilter(key, value) {
     const newQuery = {
       ...query,
-      [key]: without(query[key], value)
-    }
-    updateQuery(newQuery)
+      [key]: without(query[key], value),
+    };
+    updateQuery(newQuery);
   }
 
   const activeFilters = omit(query, 'q', 'page', 'sort');
@@ -30,34 +32,35 @@ export function FilterSummary({ schema }) {
 
   return (
     <div className={styles.filterSummary}>
-      {
-        Object.keys(activeFilters).map((key, index) => {
+      {Object.keys(activeFilters)
+        .filter((key) => key !== 'mandated') // don't show mandated widget
+        .map((key, index) => {
           let filters = activeFilters[key];
-          const settings = schema.dataset_fields.find(f => f.field_name === key);
+          const settings = schema.dataset_fields.find(
+            (f) => f.field_name === key
+          );
           if (!Array.isArray(filters)) {
-            filters = [filters]
+            filters = [filters];
           }
           return (
             <div key={key} className={styles.filterSection}>
               <h4>
-                { index > 0 && 'and '}
+                {index > 0 && 'and '}
                 {settings.label}
               </h4>
-              {
-                filters.map((filter, i) => {
-                  return (
-                    <span key={i}>
-                      {i > 0 && <span className={styles.and}>and</span>}
-                      <Widget onClick={() => removeFilter(key, filter)}>{filter}</Widget>
-                    </span>
-                  )
-                })
-              }
+              {filters.map((filter, i) => {
+                return (
+                  <span key={i}>
+                    {i > 0 && <span className={styles.and}>and</span>}
+                    <Widget onClick={() => removeFilter(key, filter)}>
+                      {filter}
+                    </Widget>
+                  </span>
+                );
+              })}
             </div>
-          )
-        })
-      }
+          );
+        })}
     </div>
-
-  )
+  );
 }
