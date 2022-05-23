aaf5ebe7ee7e1efbed7d4053440266ae3819df54
Ralph Cowling <ralph@ralph-cowling.com>
2022-05-23 13:28:29 +0200

    comment

--- ui/components/Dataset/__tests__/dataset.test.js
+++ ui/components/Dataset/__tests__/dataset.test.js
@@ -1,7 +1,6 @@
 import { formatDate } from '../index';
 
-// "2021-12-22T13:24:22.310661"
-// "2022-05-13T10:46:49.247745"
+// Results come back from CKAN in format  "2022-05-13T10:46:49.247745"
 describe('formatDate', () => {
   test('is in format 1 Jan 1970', () => {
     expect(formatDate('2021-12-22T13:24:22.310661')).toEqual('22 Dec 2021');
