4e3b76ca71aae05ded58df483b7f1a54b4bdfd74
Ralph Cowling <ralph@ralph-cowling.com>
2022-05-27 12:13:28 +0200

    add labels to search button when using icon

--- ui/components/Search/index.js
+++ ui/components/Search/index.js
@@ -15,12 +15,19 @@ const SearchIcon = () => (
     aria-hidden="true"
     focusable="false"
   >
-    <path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path>
+    <path
+      role="img"
+      alt="magnifying glass"
+      d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"
+    ></path>
   </svg>
 );
 
 const SearchButton = ({ style, children }) => (
-  <button className={classnames('nhsuk-search__submit', style)}>
+  <button
+    aria-label="search button"
+    className={classnames('nhsuk-search__submit', style)}
+  >
     {children}
   </button>
 );
