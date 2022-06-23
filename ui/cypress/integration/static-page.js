31556910f10b78431ab31e33307750edf51cc31e
Ralph Cowling <ralph@ralph-cowling.com>
2022-06-23 15:03:24 +0200

    Switch all "exactly 1" assertions to "at least 1"
    - Should get us out of the woods with integration test failures
      happening occuring when data changes and we get more results for
      specific pathways

--- ui/cypress/integration/static-page.js
+++ ui/cypress/integration/static-page.js
@@ -3,7 +3,7 @@ describe('Static page', () => {
     it('Can search from the nav search', () => {
       cy.visit('/community');
       cy.doSearch('allergies');
-      cy.get('#browse-results li').should('have.length', 1);
+      cy.get('#browse-results li').should('have.length.of.at.least', 1);
     });
   });
 });
