b0b02f11b892a450443f63c6f2e8de56f38cd8ae
Joe Fitter <hello@joefitter.com>
2022-06-29 14:51:12 +0200

    Feature - roadmap
    * Added responsive table component
    * Updated sort to use consistent format
    * Updated filters to use simpler logic
    * Added roadmap page which displayed all inactive items

---
+++ ui/components/Modal/index.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+import classnames from 'classnames';
+import styles from './Modal.module.scss'
+
+export function Modal({ trigger = 'Open modal', children, className, closeLabel = 'X', closeButton }) {
+  const [open, setOpen] = useState(false);
+
+  function toggleOpen(e) {
+    e.preventDefault();
+    setOpen(!open)
+  }
+
+  useEffect(() => {
+    if (open) {
+      document.body.classList.add('modal-open')
+    } else {
+      document.body.classList.remove('modal-open')
+    }
+  }, [open])
+
+  return (
+    <div className={classnames('modal-wrapper', className)}>
+      <p><a href="#" onClick={toggleOpen}>{trigger}</a></p>
+      {
+        open && (
+          <div className={styles.modal}>
+            <a href="#" className={styles.close} onClick={toggleOpen}>{closeLabel}</a>
+            { children }
+            {
+              closeButton && <button className={classnames('nhsuk-button', styles.closeButton)} onClick={toggleOpen}>{closeButton}</button>
+            }
+          </div>
+        )
+      }
+    </div>
+  )
+}
