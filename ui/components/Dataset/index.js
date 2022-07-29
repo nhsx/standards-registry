00319ac7bf12b51940f809dda2de74c8f3078c79
Ralph Cowling <ralph@ralph-cowling.com>
2022-07-29 16:57:53 +0200

    give role=status to results span

--- ui/components/Dataset/index.js
+++ ui/components/Dataset/index.js
@@ -165,14 +165,7 @@ const NoResultsSummary = ({ searchTerm }) => (
 const ResultSummary = ({ count, searchTerm, filtersSelected, loading }) => (
   <h2 id="resultSummary" data-loading={loading}>
     {(loading && 'Searching for results') || (
-      <Snippet
-        num={count}
-        plural={count > 1 || count === 0}
-        searchTerm={searchTerm}
-        inline
-      >
-        {searchTerm || filtersSelected ? 'filters.summary' : 'filters.all'}
-      </Snippet>
+      <span role="status">{`${count} result${count === 1 ? '' : 's'}`}</span>
     )}
   </h2>
 );
