9f1b46749306a1be45a7ee077ae521343f377d80
Ralph Cowling <ralph@ralph-cowling.com>
2022-08-23 14:44:15 +0200

    move gtag to helpers file

---
+++ ui/helpers/gtag.js
@@ -0,0 +1,16 @@
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url) => {
+  console.log('trying to send', url, GA_TRACKING_ID);
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
