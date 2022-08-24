a2cf6cc5f26281aadd6c6ad1bb27b3e61890d909
Ralph Cowling <ralph@ralph-cowling.com>
2022-08-24 16:53:45 +0200

    kitchen sink approach to adding env variables

--- ui/next.config.js
+++ ui/next.config.js
@@ -8,4 +8,8 @@ module.exports = {
   devIndicators: {
     buildActivityPosition: 'bottom-right',
   },
+  env: {
+    NEXT_PUBLIC_TRACKING_ID: process.env.NEXT_PUBLIC_TRACKING_ID,
+    NEXT_PUBLIC_TAG_ID: process.env.NEXT_PUBLIC_TAG_ID,
+  },
 };
