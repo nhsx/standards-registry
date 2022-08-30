6964abeab96f28fc970c3c0839ec6f3e6144c17a
Joe Fitter <hello@joefitter.com>
2022-08-30 16:24:32 +0200

    fix lint

--- ui/pages/future-standards.js
+++ ui/pages/future-standards.js
@@ -6,7 +6,10 @@ export default function RoadmapPlaceholder() {
     <Reading>
       <h1>Page coming soon</h1>
       <p>This page will be available soon.</p>
-      <p>In the meantime if you have any questions about future standards, email <a href={`mailto:${email}`}>{email}</a>.</p>
+      <p>
+        In the meantime if you have any questions about future standards, email{' '}
+        <a href={`mailto:${email}`}>{email}</a>.
+      </p>
     </Reading>
-  )
+  );
 }
