5420bcef8a3a34332f77c1ea48696caa8dc007ac
Joe Fitter <hello@joefitter.com>
2022-09-16 15:00:00 +0200

    added label to header search - this is visually hidden

--- ui/components/Layout/index.js
+++ ui/components/Layout/index.js
@@ -113,7 +113,7 @@ export default function Home({ children, ...props }) {
               </button>
             </div>
             {!props.hideBannerSearch && (
-              <Search label={false} placeholder="Search" location="nav" />
+              <Search placeholder="Search" location="nav" />
             )}
           </div>
         </div>
