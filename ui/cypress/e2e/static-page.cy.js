0e56a6e0be7f4648a489298dbca3edad8733dca0
Joe Fitter <hello@joefitter.com>
2022-09-27 15:29:58 +0200

    Bugfix - nested lists
    * Added a11y to test a static page - regression for header search input label
    * Reformatted TOC list to valid HTML - <ul> cannot be a child of <ul>

--- ui/cypress/e2e/static-page.cy.js
+++ ui/cypress/e2e/static-page.cy.js
@@ -1,7 +1,11 @@
+import { a11yLog, failLevel } from '../support/custom';
+
 describe('Static page', () => {
   describe('Search', () => {
     it('Can search from the nav search', () => {
       cy.visit('/help-and-resources');
+      cy.injectAxe();
+      cy.checkA11y(null, null, a11yLog, failLevel);
       cy.doSearch('allergies');
       cy.get('#browse-results li').should('have.length.of.at.least', 1);
     });
