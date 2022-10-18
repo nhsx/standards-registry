7d542325f7302a46c8ef241d7a8662421042a939
Ralph Cowling <ralph@ralph-cowling.com>
2022-10-18 14:54:36 +0200

    include contactPoint in test

--- ui/cypress/e2e/schema.org.cy.js
+++ ui/cypress/e2e/schema.org.cy.js
@@ -14,6 +14,10 @@ describe('Schema.org representations', () => {
           expect(parseJsonSchema(jsonSchema)).to.deep.equal({
             '@context': 'https://schema.org/',
             '@type': 'WebPage',
+            contactPoint: {
+              '@type': 'ContactPoint',
+              email: 'england.interop.standards@nhs.net',
+            },
             title: 'Help and resources',
             description:
               'Explore resources for the data standards community in health and social care including links to discussion forums and government regulations.',
