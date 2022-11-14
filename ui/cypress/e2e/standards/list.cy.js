4d2764c121965ef28c1cb5a916598db1f58e0211
Ralph Cowling <ralph@ralph-cowling.com>
2022-11-14 11:51:21 +0100

    match nhs for organisation searching

--- ui/cypress/e2e/standards/list.cy.js
+++ ui/cypress/e2e/standards/list.cy.js
@@ -146,21 +146,21 @@ describe('Standards Listing Index', () => {
 
         cy.get('#browse-results li a').eq(0).click();
 
-        cy.contains('dd', 'NHS Digital');
+        cy.contains('dd', 'NHS');
 
         cy.visit('/published-standards');
 
         cy.doSearch('nhsx');
         cy.get('#browse-results li a').eq(0).click();
 
-        cy.contains('dd', 'NHS Digital');
+        cy.contains('dd', 'NHS');
 
         cy.visit('/published-standards');
 
         cy.doSearch('nhs digital');
         cy.get('#browse-results li a').eq(0).click();
 
-        cy.contains('dd', 'NHS Digital');
+        cy.contains('dd', 'NHS');
       });
     });
   });
