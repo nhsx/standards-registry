a50045499594acf6fa0673480d5adca4c8cc229b
Stuart Last <stuart@metadataworks.co.uk>
2023-06-29 16:51:16 +0100

    NSDV-142 fix styling issue

--- ui/components/MarkdownBlock/index.js
+++ ui/components/MarkdownBlock/index.js
@@ -1,9 +1,10 @@
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
+import style from './MarkdownBlock.module.css';
 
 export default function MarkdownBlock({ md }) {
   return (
-    <div className="nhsuk-u-font-size-16">
+    <div className={`nhsuk-u-font-size-16 ${style.markdown}`}>
       <ReactMarkdown rehypePlugins={[rehypeRaw]}>{md}</ReactMarkdown>
     </div>
   );
