b115bbdac39d6e65cf34db0403a264bd61579801
Stuart Last <stuart@metadataworks.co.uk>
2023-07-06 16:52:42 +0100

    EX-12089 improved handling of html in markdown

--- ui/components/MarkdownBlock/index.js
+++ ui/components/MarkdownBlock/index.js
@@ -2,10 +2,20 @@ import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import style from './MarkdownBlock.module.css';
 
+function replacer(matched) {
+  const newStr = decodeURIComponent(matched.replace(/\+/g, ' '));
+  return newStr;
+}
+
 export default function MarkdownBlock({ md }) {
+  console.log('markdown', md);
+
+  const linkRegex = /(?<=href=\")[^\"]+/g;
+  const decodedMd = md.replace(linkRegex, replacer);
+
   return (
     <div className={`nhsuk-u-font-size-16 ${style.markdown}`}>
-      <ReactMarkdown rehypePlugins={[rehypeRaw]}>{md}</ReactMarkdown>
+      <ReactMarkdown rehypePlugins={[rehypeRaw]}>{decodedMd}</ReactMarkdown>
     </div>
   );
 }
