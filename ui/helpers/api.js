d6ba342d1edc8da8bbd599a1c9a0016c90f48713
ivan.tsiupa <ivan.tsupa111@gmail.com>
2023-07-18 17:14:53 +0300

    Add another prefix for reference code

--- ui/helpers/api.js
+++ ui/helpers/api.js
@@ -56,7 +56,7 @@ function getSearchQuery(q) {
     return q.slice(searchPrefix.length);
   }
 
-  const pattern =/^(DAPB|ISB)\s?[0-9]{4}\s?((Amd*|-)?\s?\d{2,4}\/?\d{0,4})?$/i
+  const pattern =/^(DAPB|ISB|DCB|SCCI)\s?[0-9]{4}\s?((Amd*|-)?\s?\d{2,4}\/?\d{0,4})?$/i
   const isMatch = pattern.test(q);
   if (isMatch) {
     return `reference_code:"${q}"`;
